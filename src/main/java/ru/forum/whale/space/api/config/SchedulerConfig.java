068aae3f049961ee7f87f37e80b0cf25bdf5ea14
Laptop <rubin3215@gmail.com>
2025-06-29 19:33:35 +0300

    Feat: implement WebSocket heartbeat via TaskScheduler

---
+++ src/main/java/ru/forum/whale/space/api/config/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package ru.forum.whale.space.api.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("ws-heartbeat-scheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
--- src/main/java/ru/forum/whale/space/api/config/WebSocketConfig.java
+++ src/main/java/ru/forum/whale/space/api/config/WebSocketConfig.java
@@ -1,18 +1,25 @@
 package ru.forum.whale.space.api.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class
 WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final ThreadPoolTaskScheduler taskScheduler;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chat", "/discussion", "/queue");
+        registry.enableSimpleBroker("/chat", "/discussion", "/queue")
+                .setTaskScheduler(taskScheduler)
+                .setHeartbeatValue(new long[]{10_000, 10_000});
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
     }
